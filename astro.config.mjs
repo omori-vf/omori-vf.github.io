986039018fae87ad472177888d8a7262a117744d
Kristype <kristianw87@gmail.com>
2023-01-23 11:10:04 +0100

    removed wrong base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   site: "https://omori-vf.github.io/",
-  base: "/omori-vf.github.io",
   integrations: [astroI18next()],
 });
