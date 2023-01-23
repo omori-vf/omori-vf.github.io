c6154f7ced2a5e4273c1e46bf7ea5cdd41abb005
Kristype <kristianw87@gmail.com>
2023-01-24 00:05:58 +0100

    added svelte support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://omori-vf.github.io/",
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), svelte()]
 });
