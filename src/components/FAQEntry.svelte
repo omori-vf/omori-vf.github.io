deaa8dfe62d51be9516c1918abd0eeb9b0a5bc12
Kristype <kristianw87@gmail.com>
2023-01-27 23:24:23 +0100

    made styles consistent across pages

--- src/components/FAQEntry.svelte
+++ src/components/FAQEntry.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    export let question: string;
-    export let answer: string;
+  export let question: string;
+  export let answer: string;
 </script>
 
-<li>
-    <strong>{question}</strong>
-    <br />
-    {@html answer}
+<li class="faq-list-item">
+  <strong>{question}</strong>
+  <br />
+  {@html answer}
 </li>
