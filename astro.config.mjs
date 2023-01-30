c199ffd7f1232b60339d62622608c06cb91b5293
Kristype <kristianw87@gmail.com>
2023-01-30 23:38:38 +0100

    added compress plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import astroI18next from "astro-i18next";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.omori-en-francais.com/",
-  integrations: [astroI18next(), svelte()],
+  integrations: [astroI18next(), svelte(), compress()]
 });
