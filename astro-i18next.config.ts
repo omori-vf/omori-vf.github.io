df1e5a4240bab1c9c040f36598c5d0a7749dedc7
Kristype <kristianw87@gmail.com>
2023-02-12 21:49:35 +0100

    removed workaround that is no longer necessary

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,13 +3,6 @@ import type { AstroI18nextConfig } from "astro-i18next";
 const config: AstroI18nextConfig = {
   defaultLocale: "fr",
   locales: ["fr"],
-  load: ["server"],
-  i18nextServer: {
-    debug: true,
-    backend: {
-      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
-    },
-  },
 };
 
 export default config;
